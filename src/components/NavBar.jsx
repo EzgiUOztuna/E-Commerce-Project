21dfb2cb6240fe06d2a3acc8e2b3ad72ab55c742
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-04-10 12:50:10 +0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
+import '../CssFolders/NavBar.css';
+
 export default function NavBar() {
     return <>
         <div>-NavBar</div>
-        <div style={{ backgroundColor: "#252B42", height: "3.62rem" }}>
+        <div className="navbar-dark" style={{ backgroundColor: "#252B42", height: "3.62rem" }}>
             <img src="src/assets/phone.svg"></img>
-            <p style={{ color: "#FFFFFF", fontFamily: "Montserrat" }}>(225) 555-0118</p>
+            <p className="phone-number" style={{ color: "#FFFFFF", fontFamily: "Montserrat" }}>(225) 555-0118</p>
         </div>
 
     </>
