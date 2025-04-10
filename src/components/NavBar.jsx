01739a1dce4fa11ed5c3aac94c3d4a594eb0c06a
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-04-10 14:02:52 +0300

    navbar dark done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,39 @@ import '../CssFolders/NavBar.css';
 
 export default function NavBar() {
     return <>
-        <div>-NavBar</div>
-        <div className="navbar-dark" style={{ backgroundColor: "#252B42", height: "3.62rem" }}>
-            <img src="src/assets/phone.svg"></img>
-            <p className="phone-number" style={{ color: "#FFFFFF", fontFamily: "Montserrat" }}>(225) 555-0118</p>
+        <div className='navbar-container'>-NavBar
+            <div className='navbar-dark'>
+                <div className='navbar-col-left'>
+                    <div className='navbar-phone'>
+                        <img className='phone-img ' src="src/assets/phone.svg"></img>
+                        <h6 className='phone-number'>(225) 555-0118</h6>
+                    </div>
+                    <div className='navbar-email'>
+                        <img className='email-img ' src="src/assets/email.svg"></img>
+                        <h6 className='email-address'>michelle.rivera@example.com</h6>
+                    </div>
+                </div>
+
+                <div className='navbar-col-middle'>
+                    <h6>Follow Us  and get a chance to win 80% off</h6>
+                </div>
+
+                <div className='navbar-col-right'>
+                    <h6>Follow Us : </h6>
+                    <div className='social-media'>
+                        <a><img src='src/assets/instagram.svg'></img></a>
+                        <a><img src='src/assets/youtube.svg'></img></a>
+                        <a><img src='src/assets/facebook.svg'></img></a>
+                        <a><img src='src/assets/twitter.svg'></img></a>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className='navbar-light'>
+
+            </div>
+
         </div>
 
     </>
