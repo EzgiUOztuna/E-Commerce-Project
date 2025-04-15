768760ee787dad0e69fe69104bcedf346aeb1362
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-15 11:31:51 +0300

    creating carousels

--- src/components/ContainerShop.jsx
+++ src/components/ContainerShop.jsx
@@ -1,3 +1,17 @@
+import '../CssFolders/ContainerShop.css';
+
 export default function ContainerShop() {
-    return <div>-ContainerShop</div>;
+    return <>
+        <div className="carousel">
+            <button className="control-prev"><img src='src/assets/carousel-control-prev.svg'></img></button>
+            <button className="control-next"><img src='src/assets/carousel-control-next.svg'></img></button>
+            <div className='inner-container'>
+                <h5>SUMMER 2020</h5>
+                <h1>NEW COLLECTION</h1>
+                <h4>We know how large objects will act,
+                    but things on a small scale.</h4>
+                <button><h3>SHOP NOW</h3></button>
+            </div>
+        </div>
+    </>
 }
