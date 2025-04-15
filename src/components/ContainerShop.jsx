0166297a3329cfa85ac170d9c6b832b1d1a0536e
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-15 14:04:37 +0300

    updates carousel1

--- src/components/ContainerShop.jsx
+++ src/components/ContainerShop.jsx
@@ -1,20 +1,38 @@
-import { useState } from 'react';
-import ExampleCarouselImage from './subcomponents/ExampleCarouselImage.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Carousel } from 'react-bootstrap';
-import img1 from "../assets/carousel1.svg";
-import img2 from "../assets/carousel1.svg";
-import img3 from "../assets/carousel1.svg";
+import '../CssFolders/ContainerShop.css';
+//import ExampleCarouselImage from './subcomponents/ExampleCarouselImage.jsx';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import { Carousel } from 'react-bootstrap';
+//import img1 from "../assets/carousel1.svg";
+//import img2 from "../assets/carousel1.svg";
+//import img3 from "../assets/carousel1.svg";
 
-function ContainerShop() {
-    const [index, setIndex] = useState(0);
+export default function ContainerShop() {
 
-    const handleSelect = (selectedIndex) => {
-        setIndex(selectedIndex);
-    };
 
-    return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
+    return <>
+        <div className='carousels'>
+            <div className="carousel-1">
+                <button className="control-prev"><img src='src/assets/carousel-control-prev.svg'></img></button>
+                <button className="control-next"><img src='src/assets/carousel-control-next.svg'></img></button>
+                <div className='inner-container'>
+                    <h5>SUMMER 2020</h5>
+                    <h1>NEW COLLECTION</h1>
+                    <h4>We know how large objects will act,
+                        but things on a small scale.</h4>
+                    <button><h3>SHOP NOW</h3></button>
+                </div>
+            </div>
+        </div>
+    </>
+}
+
+
+
+
+
+
+/*
+<Carousel activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item>
                 <ExampleCarouselImage src={img1} text="First slide" />
                 <Carousel.Caption>
@@ -25,9 +43,10 @@ function ContainerShop() {
                     <button><h3>SHOP NOW</h3></button>
                 </Carousel.Caption>
             </Carousel.Item>
+
             <Carousel.Item>
                 <ExampleCarouselImage src={img2} text="Second slide" />
-                <Carousel.Caption>
+                <Carousel.Caption >
                     <h5>SUMMER 2020</h5>
                     <h1>NEW COLLECTION</h1>
                     <h4>We know how large objects will act,
@@ -45,29 +64,6 @@ function ContainerShop() {
                     <button><h3>SHOP NOW</h3></button>
                 </Carousel.Caption>
             </Carousel.Item>
+
         </Carousel>
-    );
-}
-
-export default ContainerShop;
-
-
-
-
-/*import '../CssFolders/ContainerShop.css';
-
-export default function ContainerShop() {
-    return <>
-        <div className="carousel">
-            <button className="control-prev"><img src='src/assets/carousel-control-prev.svg'></img></button>
-            <button className="control-next"><img src='src/assets/carousel-control-next.svg'></img></button>
-            <div className='inner-container'>
-                <h5>SUMMER 2020</h5>
-                <h1>NEW COLLECTION</h1>
-                <h4>We know how large objects will act,
-                    but things on a small scale.</h4>
-                <button><h3>SHOP NOW</h3></button>
-            </div>
-        </div>
-    </>
-}*/
+*/
