1dd1cdf3ea2d11a3ccb168b6fc4935171303900d
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-15 15:10:12 +0300

    updates carousels

---
+++ src/assets/data.js
@@ -0,0 +1,24 @@
+// ../assets/data.js
+export const carouselData = [
+    {
+        bgImage: "src/assets/carousel1.svg",
+        h5: "SUMMER 2020",
+        h1: "NEW COLLECTION",
+        h4: "We know how large objects will act, but things on a small scale.",
+        h3: "SHOP NOW",
+    },
+    {
+        bgImage: "src/assets/carousel1.svg",
+        h5: "WINTER 2020",
+        h1: "COZY COLLECTION",
+        h4: "Everything you need to stay warm this winter.",
+        h3: "SHOP NOW",
+    },
+    {
+        bgImage: "src/assets/carousel1.svg",
+        h5: "SPRING 2021",
+        h1: "FRESH LOOK",
+        h4: "Time for new beginnings.",
+        h3: "SHOP NOW",
+    }
+];
--- src/components/ContainerShop.jsx
+++ src/components/ContainerShop.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { carouselData } from '../assets/data';
 import '../CssFolders/ContainerShop.css';
 //import ExampleCarouselImage from './subcomponents/ExampleCarouselImage.jsx';
 //import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,22 +9,47 @@ import '../CssFolders/ContainerShop.css';
 //import img3 from "../assets/carousel1.svg";
 
 export default function ContainerShop() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const totalSlides = carouselData.length;
+
+    const handlePrev = () => {
+        setCurrentIndex(prev => (prev - 1 + totalSlides) % totalSlides);
+    };
+
+    const handleNext = () => {
+        setCurrentIndex(prev => (prev + 1) % totalSlides);
+    };
 
 
     return <>
         <div className='carousels'>
-            <div className="carousel-1">
-                <button className="control-prev"><img src='src/assets/carousel-control-prev.svg'></img></button>
-                <button className="control-next"><img src='src/assets/carousel-control-next.svg'></img></button>
-                <div className='inner-container'>
-                    <h5>SUMMER 2020</h5>
-                    <h1>NEW COLLECTION</h1>
-                    <h4>We know how large objects will act,
-                        but things on a small scale.</h4>
-                    <button><h3>SHOP NOW</h3></button>
+            {carouselData.map((item, index) => (
+                <div key={index}
+                    className={`carousel-slide ${currentIndex === index ? 'active' : 'hidden'}`}
+                    style={{
+                        backgroundImage: `url(${item.bgImage})`,
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat'
+                    }} >
+                    <button className="control-prev" onClick={handlePrev}>
+                        <img src='src/assets/carousel-control-prev.svg'></img>
+                    </button>
+                    <button className="control-next" onClick={handleNext}>
+                        <img src='src/assets/carousel-control-next.svg'></img>
+                    </button>
+                    <div className='inner-container'>
+                        <h5>{item.h5}</h5>
+                        <h1>{item.h1}</h1>
+                        <h4>{item.h4}</h4>
+                        <button><h3>{item.h3}</h3></button>
+                    </div>
                 </div>
-            </div>
-        </div>
+            ))}
+
+
+
+
+        </div >
     </>
 }
 
