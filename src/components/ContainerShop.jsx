309d9e05f423821dc0479941a03f8b6dae5045da
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-15 12:26:57 +0300

    updated carousel

--- src/components/ContainerShop.jsx
+++ src/components/ContainerShop.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import ExampleCarouselImage from '../components/ExampleCarouselImage.jsx';
+import ExampleCarouselImage from './subcomponents/ExampleCarouselImage.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Carousel } from 'react-bootstrap';
 import img1 from "../assets/carousel1.svg";
 import img2 from "../assets/carousel1.svg";
@@ -25,19 +26,23 @@ function ContainerShop() {
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <ExampleCarouselImage src={img1} text="Second slide" />
+                <ExampleCarouselImage src={img2} text="Second slide" />
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h5>SUMMER 2020</h5>
+                    <h1>NEW COLLECTION</h1>
+                    <h4>We know how large objects will act,
+                        but things on a small scale.</h4>
+                    <button><h3>SHOP NOW</h3></button>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <ExampleCarouselImage src={img1} text="Third slide" />
+                <ExampleCarouselImage src={img3} text="Third slide" />
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
+                    <h5>SUMMER 2020</h5>
+                    <h1>NEW COLLECTION</h1>
+                    <h4>We know how large objects will act,
+                        but things on a small scale.</h4>
+                    <button><h3>SHOP NOW</h3></button>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
