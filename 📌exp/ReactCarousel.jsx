3e806938215e2c140e12d5e54d27116f6fd1170c
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-17 12:46:31 +0300

    Create ReactCarousel.jsx

---
+++ 📌exp/ReactCarousel.jsx
@@ -0,0 +1,44 @@
+//import ExampleCarouselImage from './subcomponents/ExampleCarouselImage.jsx';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import { Carousel } from 'react-bootstrap';
+//import img1 from "../assets/carousel1.svg";
+//import img2 from "../assets/carousel1.svg";
+//import img3 from "../assets/carousel1.svg";
+
+
+/*
+<Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <ExampleCarouselImage src={img1} text="First slide" />
+                <Carousel.Caption>
+                    <h5>SUMMER 2020</h5>
+                    <h1>NEW COLLECTION</h1>
+                    <h4>We know how large objects will act,
+                        but things on a small scale.</h4>
+                    <button><h3>SHOP NOW</h3></button>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <ExampleCarouselImage src={img2} text="Second slide" />
+                <Carousel.Caption >
+                    <h5>SUMMER 2020</h5>
+                    <h1>NEW COLLECTION</h1>
+                    <h4>We know how large objects will act,
+                        but things on a small scale.</h4>
+                    <button><h3>SHOP NOW</h3></button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <ExampleCarouselImage src={img3} text="Third slide" />
+                <Carousel.Caption>
+                    <h5>SUMMER 2020</h5>
+                    <h1>NEW COLLECTION</h1>
+                    <h4>We know how large objects will act,
+                        but things on a small scale.</h4>
+                    <button><h3>SHOP NOW</h3></button>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+        </Carousel>
+*/
