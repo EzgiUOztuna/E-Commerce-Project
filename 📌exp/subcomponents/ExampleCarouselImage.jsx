f7436b9fbcfff7ab73dcb0104b06498ea4d657a7
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-17 12:46:28 +0300

    Create ExampleCarouselImage.jsx

---
+++ 📌exp/subcomponents/ExampleCarouselImage.jsx
@@ -0,0 +1,14 @@
+// ExampleCarouselImage.jsx
+function ExampleCarouselImage({ src, text }) {
+    return (
+        <img
+            className="d-block w-100"
+            src={src}
+            alt={text}
+        />
+    );
+}
+
+export default ExampleCarouselImage;
+
+//NOT USING RIGHT NOW ❌
