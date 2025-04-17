b0849fa1dc9f39f588585e55ab9b786b9fd903d6
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-17 13:48:17 +0300

    Update ContainerShop.jsx

--- src/components/ContainerShop.jsx
+++ src/components/ContainerShop.jsx
@@ -27,12 +27,17 @@ export default function ContainerShop() {
                                    lg:top-[20rem] lg:left-[2%] lg:cursor-pointer" onClick={handlePrev}> {/*control-prev*/}
                     <img src='src/assets/icons/carousel-control-prev.svg'></img>
                 </button>
-                <div className='lg:w-[37.44rem] lg:h-[20.68rem] lg:flex lg:flex-col lg:gap-9 lg:pt-28 lg:pb-28 lg:ml-[12.5rem]'> {/*inner-container*/}
-                    <h5 className='lg:font-montserrat lg:font-bold lg:text-base lg:leading-6 lg:tracking-[0.1px] lg:m-0 lg:text-[#FFFFFF]'>{item.h5}</h5>
-                    <h1 className='lg:font-montserrat lg:font-bold lg:text-[3.625rem] lg:leading-[5rem] lg:tracking-[0.2px] lg:m-0 lg:text-[#FFFFFF]'>{item.h1}</h1>
-                    <h4 className='lg:w-[23.5rem] lg:font-montserrat lg:font-normal lg:text-xl lg:leading-[1.875rem] lg:tracking-[0.0125rem] lg:text-[#FAFAFA] lg:m-0'>{item.h4}</h4>
-                    <button className='lg:w-[14.25rem] lg:h-[3.875rem] lg:gap-[0.625rem] lg:pt-[0.9375rem] lg:pr-10 lg:pb-[0.9375rem] lg:pl-10 lg:rounded-[0.3125rem] lg:bg-[#2DC071] lg:border-none lg:cursor-pointer'>
-                        <h3 className='lg:font-montserrat lg:font-bold lg:text-base lg:leading-8 lg:tracking-[0.00625rem] lg:text-center lg:m-0 lg:text-white'>{item.h3}</h3>
+                <div className='flex flex-col items-center gap-[2.18rem] pt-[10rem]
+                                lg:w-[37.44rem] lg:h-[20.68rem] lg:items-start lg:gap-9 lg:pt-28 lg:pb-28 lg:ml-[12.5rem]'> {/*inner-container*/}
+                    <h5 className='font-montserrat font-bold text-base leading-6 tracking-[0.1px] m-0 text-[#FFFFFF]
+                                   '>{item.h5}</h5>
+                    <h1 className='font-montserrat font-bold text-4xl leading-[3.125rem] tracking-[0.2px] lg:m-0 text-[#FFFFFF] w-[16.75rem] text-center
+                                   lg:text-[3.625rem] lg:leading-[5rem] lg:w-[35.62rem] lg:text-left'>{item.h1}</h1>
+                    <h4 className='w-[13rem] font-montserrat font-normal text-sm leading-[1.875rem] tracking-[0.2px] text-center text-[#FAFAFA] m-0
+                                    lg:w-[23.5rem] lg:text-xl lg:leading-[1.875rem] lg:tracking-[0.0125rem]'>{item.h4}</h4>
+                    <button className='w-[13.813rem] h-[3.875rem] gap-[0.625rem] bg-[#2DC071] prx-10 rounded-[0.3125rem] border-none
+                                      lg:w-[14.25rem] lg:pt-[0.9375rem] lg:pb-[0.9375rem] lg:cursor-pointer'>
+                        <h3 className='font-montserrat font-bold text-base leading-8 tracking-[0.00625rem] text-center m-0 text-white'>{item.h3}</h3>
                     </button>
                 </div>
                 <button className="absolute w-6 h-[2.78rem] top-[52rem] left-[22.5rem]
