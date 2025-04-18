289d6617c68b6faf6354bd9d9062c5954554b454
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-18 13:44:45 +0300

    Update shopCardsData.js

--- src/assets/dataStore/shopCardsData.js
+++ src/assets/dataStore/shopCardsData.js
@@ -2,17 +2,37 @@ export const shopCardsData = [
     {
         category: "MEN",
         desktopImage: "src/assets/images/men-bg-cover.svg",
-        mobileImage: "src/assets/images/men-bg-cover-mobile.svg"
+        mobileImage: "src/assets/images/men-bg-cover-mobile.svg",
+        styleDesktop: {
+            width: "510px",
+            height: "500px",
+            backgroundImage: "url(src/assets/images/men-bg-cover.svg)",
+        },
+        styleMobile: {
+            width: "324px",
+            height: "500px",
+            backgroundImage: "src/assets/images/men-bg-cover-mobile.svg)",
+        },
     },
     {
         category: "WOMEN",
         desktopImage: "src/assets/images/women-bg-cover.svg",
-        mobileImage: "src/assets/images/women-bg-cover-mobile.svg"
+        mobileImage: "src/assets/images/women-bg-cover-mobile.svg",
+        styleDesktop: {
+            width: "240px",
+            height: "500px",
+            backgroundImage: "url(src/assets/images/women-bg-cover.svg)",
+        },
+        styleMobile: {
+            width: "325px",
+            height: "500px",
+            backgroundImage: "url(src/assets/images/women-bg-cover-mobile.svg)",
+        }
     },
     {
         category: "ACCESSORIES",
         desktopImage: "src/assets/images/accessories-bg-cover.svg",
-        mobileImage: "src/assets/images/accessories-bg-cover-mobile.svg"
+        mobileImage: "src/assets/images/accesseroies-bg-cover-mobile.svg"
     },
     {
         category: "KIDS",
