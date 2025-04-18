b9b40a013efb349ed24a4209146b879cc20c4d67
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-18 13:04:38 +0300

    Create shopCardsData.js

---
+++ src/assets/dataStore/shopCardsData.js
@@ -0,0 +1,22 @@
+export const shopCardsData = [
+    {
+        category: "MEN",
+        desktopImage: "src/assets/images/men-bg-cover.svg",
+        mobileImage: "src/assets/images/men-bg-cover-mobile.svg"
+    },
+    {
+        category: "WOMEN",
+        desktopImage: "src/assets/images/women-bg-cover.svg",
+        mobileImage: "src/assets/images/women-bg-cover-mobile.svg"
+    },
+    {
+        category: "ACCESSORIES",
+        desktopImage: "src/assets/images/accessories-bg-cover.svg",
+        mobileImage: "src/assets/images/accessories-bg-cover-mobile.svg"
+    },
+    {
+        category: "KIDS",
+        desktopImage: "src/assets/images/kids-bg-cover.svg",
+        mobileImage: "src/assets/images/kids-bg-cover-mobile.svg"
+    },
+];
