a6890a31670b9978e764753b78fca3dbed3bc692
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-21 16:22:28 +0300

    Update ProductCards.jsx

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -1,6 +1,7 @@
 import { productCardsData } from "../assets/dataStore/productCardsData"
 
 export default function ProductCards() {
+
     return <>
         {/*<div>-ProductCard</div>;*/}
         <div className="font-montserrat
@@ -18,8 +19,37 @@ export default function ProductCards() {
                         <div className="flex flex-col items-center font-bold text-center lg:pt-6 lg:pb-9 lg:gap-[0.625rem]">
                             <h5 className="text-[#252B42] lg:text-base lg:leading-6 lg:tracking-[0.1px]">{item.productTitle}</h5>
                             <a className="text-sm text-[#737373] lg:leading-6 lg:tracking-[0.2px]" href="/product-link">{item.productSubtitle}</a>
-                            <p>Price</p>
-                            <p>Colors</p>
+                            <div className="flex font-bold text-base leading-6 tracking-[0.1px] text-center 
+                                            lg:py-1 lg:p-1 lg:gap-2">
+                                <h5 className="text-[#BDBDBD]">${item.originalPrice}  </h5>
+                                <h5 className="text-[#23856D]">${item.originalPrice - (Number(item.originalPrice) - (Number(item.originalPrice) * (Number(item.discountRate) / 100))).toFixed(2)}</h5>
+                            </div>
+                            <div className="flex gap-2">
+                                <div className="w-4 h-4 border rounded-lg"
+                                    style={{
+                                        color: item.colors.firstColor,
+                                        backgroundColor: item.colors.firstColor
+                                    }} >
+                                </div>
+                                <div className="w-4 h-4 border rounded-lg"
+                                    style={{
+                                        color: item.colors.secondColor,
+                                        backgroundColor: item.colors.secondColor
+                                    }} >
+                                </div>
+                                <div className="w-4 h-4 border rounded-lg"
+                                    style={{
+                                        color: item.colors.thirdColor,
+                                        backgroundColor: item.colors.thirdColor
+                                    }} >
+                                </div>
+                                <div className="w-4 h-4 border rounded-lg"
+                                    style={{
+                                        color: item.colors.fourthColor,
+                                        backgroundColor: item.colors.fourthColor
+                                    }} >
+                                </div>
+                            </div>
                         </div>
 
                     </div>
