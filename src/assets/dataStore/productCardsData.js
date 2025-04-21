429bd166f998d8b1a663cb575b559ba9ab34f658
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-21 14:17:36 +0300

    Create productCardsData.js

---
+++ src/assets/dataStore/productCardsData.js
@@ -0,0 +1,42 @@
+export const productCardsData = [
+    {
+        productImage: "src/assets/images/productCardsImg/product-cover-1.png",
+        productTitle: "Graphic Design",
+        productSubtitle: "English Department",
+    },
+    {
+        productImage: "src/assets/images/productCardsImg/product-cover-2.png",
+        productTitle: "Graphic Design",
+        productSubtitle: "English Department",
+    },
+    {
+        productImage: "src/assets/images/productCardsImg/product-cover-3.png",
+        productTitle: "Graphic Design",
+        productSubtitle: "English Department",
+    },
+    {
+        productImage: "src/assets/images/productCardsImg/product-cover-4.png",
+        productTitle: "Graphic Design",
+        productSubtitle: "English Department",
+    },
+    {
+        productImage: "src/assets/images/productCardsImg/product-cover-5.png",
+        productTitle: "Graphic Design",
+        productSubtitle: "English Department",
+    },
+    {
+        productImage: "src/assets/images/productCardsImg/product-cover-6.png",
+        productTitle: "Graphic Design",
+        productSubtitle: "English Department",
+    },
+    {
+        productImage: "src/assets/images/productCardsImg/product-cover-7.png",
+        productTitle: "Graphic Design",
+        productSubtitle: "English Department",
+    },
+    {
+        productImage: "src/assets/images/productCardsImg/product-cover-8.png",
+        productTitle: "Graphic Design",
+        productSubtitle: "English Department",
+    },
+];
