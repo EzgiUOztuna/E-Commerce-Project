c9908cf3ba76e3718b4103c5da9341f034a5894b
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-22 18:26:51 +0300

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -18,7 +18,7 @@ export default function Content() {
         {contentPageCarouselData.map((item, index) => (
             <div key={index}
                 className={`${currentIndex === index ? 'block' : 'hidden'}
-                            bg-cover bg-no-repeat box-border
+                            h-[1230px] bg-cover bg-no-repeat
                             lg:h-[709px] 
                         `} style={{
                     backgroundImage: `url(${item.bgImage})`
@@ -27,16 +27,16 @@ export default function Content() {
                                    lg:top-[22rem] lg:left-[2%] lg:cursor-pointer" onClick={handlePrev}>
                     <img src='src/assets/icons/carousel-control-prev.svg'></img>
                 </button>
-                <div className='font-montserrat text-[#FFFFFF] flex flex-col items-center gap-[2.18rem] pt-[8rem] justify-center
+                <div className='font-montserrat text-[#FFFFFF] flex flex-col items-center gap-[2.18rem] pt-[8rem]
                                 lg:w-[37.44rem] lg:h-[20.68rem] lg:items-start lg:gap-9 lg:pt-28 lg:pb-28 lg:ml-[12.5rem]'>
                     <h4 className='font-normal text-xl leading-[1.875rem] tracking-[0.2px] m-0 
                                    '>{item.h4}</h4>
                     <h1 className='font-bold text-[2.5rem] leading-[3.125rem] tracking-[0.2px] lg:m-0 w-[16.75rem] text-center
                                    lg:w-[35.62rem] lg:text-[3.625rem] lg:leading-[5rem] lg:text-left'>{item.h1}</h1>
                     <p className='w-[18rem] font-normal text-xl leading-[1.875rem] tracking-[0.2px] m-0 text-center
-                                  lg:w-[23.5rem] lg:text-sm lg:leading-[1.25rem] lg:tracking-[0.0125rem]'>{item.text}</p>
-                    <div className="flex flex-col gap-5 font-bold
-                                    lg:gap-9 lg:items-center">
+                                  lg:w-[23.5rem] lg:text-sm lg:leading-[1.25rem] lg:tracking-[0.0125rem] lg:text-left'>{item.text}</p>
+                    <div className="flex flex-col gap-5 font-bold items-center
+                                    lg:gap-9 lg:flex-row">
                         <h3 className="text-2xl leading-8 tracking-[0.1px] text-center">${item.price}</h3>
                         <button className='w-[12.4rem] h-[3.4rem] gap-[0.625rem] bg-[#2DC071] px-10 py-[0.9375rem] rounded-[0.3125rem] border-none
                                            lg:h-[3.25rem] lg:cursor-pointer'>
