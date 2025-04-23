032e1832da8abcf198659f61a1052923e3931661
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-23 10:44:47 +0300

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,7 @@
 export default function Features() {
     return <>
         {/*<div>-Features</div>;*/}
-        <div className="flex flex-col
+        <div className="flex flex-col gap-7 pt-[7.5rem]
                         lg:flex-row lg:items-center lg:gap-8 lg:justify-center lg:h-[682px]">
             <img className="order-2 lg:order-1" src="src/assets/images/FeaturesImg/features1.png" />
             <div className="font-montserrat flex flex-col items-center gap-8 order-1 
@@ -13,11 +13,14 @@ export default function Features() {
                                lg:w-[376px] lg:text-left">We know how large objects will act, but things on a small scale.</h4>
                 <div className="flex flex-col gap-6
                                 lg:flex-row lg:gap-3 lg:h-[52px]">
-                    <button className="rounded-[5px] border-solid bg-[#2DC071] px-10 py-4 lg:w-[155.6px]">
+                    <button className="rounded-[5px] border-solid bg-[#23A6F0] px-10 py-4 
+                                     lg:bg-[#2DC071] lg:w-[155.6px]">
                         <p className="font-bold text-center text-[#FFFFFF] lg:text-sm lg:leading-6 lg:tracking-[0.2px]">BUY NOW</p>
                     </button>
-                    <button className="rounded-[5px] border border-[#2DC071] px-10 py-4 lg:w-[174.7px]">
-                        <p className="font-bold text-center text-[#2DC071] lg:text-sm lg:leading-6 lg:tracking-[0.2px]">READ MORE</p>
+                    <button className="rounded-[5px] border border-[#23A6F0] px-10 py-4 
+                                     lg:border-[#2DC071] lg:w-[174.7px]">
+                        <p className="font-bold text-center text-[#23A6F0] 
+                                    lg:text-[#2DC071] lg:text-sm lg:leading-6 lg:tracking-[0.2px]">READ MORE</p>
                     </button>
                 </div>
             </div>
