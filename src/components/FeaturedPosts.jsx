7b7d6cec58850d91ee5fddc8c08e7c2d8c3f944a
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-24 17:59:15 +0300

    Update FeaturedPosts.jsx

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -1,9 +1,17 @@
 export default function FeaturedPosts() {
     return <>
         {/*<div>-Featured Posts</div>*/}
-        <div className="flex flex-col items-center font-montserrat">
-            <h6 className="font-bold text-sm leading-6 tracking-[0.2px] text-center text-[#23A6F0]">Practice Advice</h6>
-            <h2 className="font-bold text-[2.5rem] leading-[3.125rem] tracking-[0.2px] text-center text-[#252B42]">Featured Posts</h2>
+        <div className="flex flex-col py-20 gap-20
+                        lg:py-28">
+            <div className="flex flex-col items-center font-montserrat gap-3">
+                <h6 className="font-bold text-sm leading-6 tracking-[0.2px] text-center text-[#23A6F0]">Practice Advice</h6>
+                <h2 className="font-bold text-[2.5rem] leading-[3.125rem] tracking-[0.2px] text-center text-[#252B42] w-[15rem] lg:w-[20rem]">Featured Posts</h2>
+                <p className="font-normal text-sm leading-5 tracking-[0.2px] text-center text-[#737373] w-[17rem] lg:w-[30rem]">Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
+            </div>
+
+            <div>
+
+            </div>
         </div>
     </>
 }
