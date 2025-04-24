787455ab7c9e314513a96bbc974c5ccdd412662e
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-24 17:48:20 +0300

    Update FeaturedPosts.jsx

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -1,5 +1,8 @@
 export default function FeaturedPosts() {
     return <>
-        <div>-Featured Posts</div>
+        {/*<div>-Featured Posts</div>*/}
+        <div className="flex flex-col items-center font-montserrat">
+            <h6 className="font-bold text-sm leading-6 tracking-[0.2px] text-center text-[#23A6F0]">Practice Advice</h6>
+        </div>
     </>
 }
