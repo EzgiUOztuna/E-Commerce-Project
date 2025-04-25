1a124b33158a35f431d5cf890ecf8477bc2b6b71
EzgiUOztuna <ezgiuygn@gmail.com>
2025-04-25 12:16:12 +0300

    Create featuredPostsData.js

---
+++ src/assets/dataStore/featuredPostsData.js
@@ -0,0 +1,23 @@
+export const featuredPostsData = [
+    {
+        image: "src/assets/images/featuredPostsImg/image-1.png",
+        h4: "Loudest à la Madison #1 (L'integral)",
+        def: "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+        time: "22 April 2021",
+        comments: "10 comments",
+    },
+    {
+        image: "src/assets/images/featuredPostsImg/image-2.png",
+        h4: "Loudest à la Madison #1 (L'integral)",
+        def: "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+        time: "22 April 2021",
+        comments: "10 comments",
+    },
+    {
+        image: "src/assets/images/featuredPostsImg/image-3.png",
+        h4: "Loudest à la Madison #1 (L'integral)",
+        def: "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+        time: "22 April 2021",
+        comments: "10 comments",
+    },
+];
