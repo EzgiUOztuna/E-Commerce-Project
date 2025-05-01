d825ce483f2f6759459556b2d13adc1ce6e6eb0a
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-01 17:26:12 +0300

    Update Clients.jsx

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,6 +1,21 @@
+import { clientsData } from "../assets/dataStore/clientsData";
+
 export default function Clients() {
     return <>
-        {/*<div>-Clients</div>;*/}
+        <div className="bg-[#FAFAFA] lg:h-[11rem]">
+            <div className="flex flex-col items-center py-24 gap-16
+                            lg:w-[70rem] lg:mx-auto lg:py-12 lg:flex-row lg:items-center lg:gap-8">
+                {clientsData.map((item, index) => (
+                    <div key={index}
+                        className="lg:w-[153px]">
+                        <img src={item.img}></img>
+                    </div>
+                ))}
+            </div>
+
+        </div>
+
+
 
     </>
 }
