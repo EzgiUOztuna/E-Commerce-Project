41c6a4e07bf3d0e54285e05e613439dc2122e8f8
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-01 17:25:51 +0300

    Create clientsData.js

---
+++ src/assets/dataStore/clientsData.js
@@ -0,0 +1,20 @@
+export const clientsData = [
+    {
+        img: "src/assets/images/clientsImg/fa-brands-1.svg",
+    },
+    {
+        img: "src/assets/images/clientsImg/fa-brands-2.svg",
+    },
+    {
+        img: "src/assets/images/clientsImg/fa-brands-3.svg",
+    },
+    {
+        img: "src/assets/images/clientsImg/fa-brands-4.svg",
+    },
+    {
+        img: "src/assets/images/clientsImg/fa-brands-5.svg",
+    },
+    {
+        img: "src/assets/images/clientsImg/fa-brands-6.svg",
+    },
+]
