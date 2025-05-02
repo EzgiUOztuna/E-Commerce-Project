39f632cd3d0d901c3496d8d6289862f3e90c605c
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-02 20:56:28 +0300

    Update Footer.jsx

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
         <div className="flex flex-col font-montserrat">
             <div className="bg-[#FAFAFA] font-montserrat  
                         lg:h-36 ">
-                <div className="flex flex-col mx-auto py-10 justify-between w-[20rem] gap-3
+                <div className="flex flex-col mx-auto py-10 justify-between w-[20rem] gap-3 
                             lg:flex-row lg:items-center  lg:w-[70rem] lg:gap-[36.125rem]">
                     <h3 className="font-bold text-2xl leading-8 tracking-[0.1px] text-[#252B42]">Bandage</h3>
                     <div className="flex items-center gap-5">
