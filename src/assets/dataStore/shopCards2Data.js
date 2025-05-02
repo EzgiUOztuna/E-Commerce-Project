a14039fdf56f3e8b18f1cd2b73ddd08c246bf924
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-02 19:03:45 +0300

    Create shopCards2Data.js

---
+++ src/assets/dataStore/shopCards2Data.js
@@ -0,0 +1,27 @@
+export const shopCards2Data = [
+    {
+        bgImage: "src/assets/images/shopPages2Img/media bg-cover-1.png",
+        category: "CLOTHS",
+        numberOfItems: 5,
+    },
+    {
+        bgImage: "src/assets/images/shopPages2Img/media bg-cover-2.png",
+        category: "CLOTHS",
+        numberOfItems: 5,
+    },
+    {
+        bgImage: "src/assets/images/shopPages2Img/media bg-cover-3.png",
+        category: "CLOTHS",
+        numberOfItems: 5,
+    },
+    {
+        bgImage: "src/assets/images/shopPages2Img/media bg-cover-4.png",
+        category: "CLOTHS",
+        numberOfItems: 5,
+    },
+    {
+        bgImage: "src/assets/images/shopPages2Img/media bg-cover-5.png",
+        category: "CLOTHS",
+        numberOfItems: 5,
+    },
+]
