f63a18d4dccef274e1706ed4a5c54aea7257dbb2
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-02 20:07:20 +0300

    Update ShopCards2.jsx

--- src/components/ShopCards2.jsx
+++ src/components/ShopCards2.jsx
@@ -2,11 +2,11 @@ import { shopCards2Data } from "../assets/dataStore/shopCards2Data";
 
 export default function ShopCards2() {
     return <>
-        <div className="flex flex-col bg-[#FAFAFA] mx-auto justify-center font-montserrat text-[#FFFFFF]
-                        lg:flex-row lg:pb-12 lg:gap-4 lg:w-[70rem]">
+        <div className="flex flex-col bg-[#FAFAFA] mx-auto justify-center items-center font-montserrat text-[#FFFFFF] py-6 gap-4
+                        lg:flex-row lg:pb-12 lg:pt-0 lg:w-[70rem]">
             {shopCards2Data.map((item, index) => (
                 <div key={index}
-                    className="flex flex-col justify-center items-center bg-cover 
+                    className="flex flex-col justify-center items-center bg-cover w-[20.75rem] h-[18.75rem]
                                lg:w-[13rem] lg:h-[14rem]"
                     style={{ backgroundImage: `url(${item.bgImage})` }}>
                     <h6 className="font-bold text-base leading-6 tracking-[0.1px] text-center">{item.category}</h6>
