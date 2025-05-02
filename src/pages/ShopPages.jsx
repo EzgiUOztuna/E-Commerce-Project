ac67e4f175ae9e1c28ac9110bf5bafaa3b9feb19
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-02 20:13:09 +0300

    Update ShopPages.jsx

--- src/pages/ShopPages.jsx
+++ src/pages/ShopPages.jsx
@@ -112,11 +112,11 @@ export default function ShopPages() {
                 </div>
             </div>
         </div >
-        <div className='bg-[#FAFAFA] font-montserrat py-6 lg:h-[5.75rem]'>
+        <div className='bg-[#FAFAFA] font-montserrat py-6'>
             <div className='flex flex-col items-center gap-8
                             lg:w-[70rem] lg:flex-row lg:justify-between lg:mx-auto'>
                 <h3 className='font-bold text-2xl leading-8 tracking-[0.1px] text-center text-[#252B42] py-6'>Shop</h3>
-                <div className='flex gap-4 py-3 font-bold text-sm leading-6 tracking-[0.2px] text-center'>
+                <div className='flex items-center gap-4 py-3 font-bold text-sm leading-6 tracking-[0.2px] text-center'>
                     <a href='/home' className='text-[#252B42]'>Home</a>
                     <img src='src/assets/icons/arrow-right-icn.svg' />
                     <h6 className='text-[#BDBDBD]'>Shop</h6>
