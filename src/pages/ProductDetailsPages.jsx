96e9490e5b96871fad9a0cd733e302a635dba6ac
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-05 17:27:29 +0300

    Update ProductDetailsPages.jsx

--- src/pages/ProductDetailsPages.jsx
+++ src/pages/ProductDetailsPages.jsx
@@ -1,5 +1,22 @@
+import Clients from '../components/Clients';
+import FooterLight from '../components/FooterLight';
+import SiteHeader from '../components/SiteHeader';
+
 export default function ProductDetailsPages() {
+
     return <>
-        <p>XXX</p>
+        <SiteHeader />
+        <div className='bg-[#FAFAFA] font-montserrat py-6'>
+            <div className='flex flex-col
+                            lg:w-[69rem] lg:mx-auto'>
+                <div className='flex items-center gap-4 py-3 font-bold text-sm leading-6 tracking-[0.2px] text-center'>
+                    <a href='/home' className='text-[#252B42]'>Home</a>
+                    <img src='src/assets/icons/arrow-right-icn.svg' />
+                    <h6 className='text-[#BDBDBD]'>Shop</h6>
+                </div>
+            </div>
+        </div>
+        <Clients />
+        <FooterLight />
     </>
 }
