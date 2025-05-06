14d6d24690c1efa4cd083f0b9835b06d6b3d59ef
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-06 15:29:24 +0300

    Update productDetailsData.js

--- src/assets/dataStore/productDetailsData.js
+++ src/assets/dataStore/productDetailsData.js
@@ -11,8 +11,8 @@ export const productDetailsData = [
         id: 2,
         images: [
             "src/assets/images/productDetailsImg/single-product-1-cover-2.jpg",
-            "src/assets/images/productDetailsImg/single-product-1-thumb-1.jpg",
-            "src/assets/images/productDetailsImg/single-product-1-thumb-2.jpg",
+            "src/assets/images/productDetailsImg/single-product-1-thumb-3.jpg",
+            "src/assets/images/productDetailsImg/single-product-1-thumb-4.jpg",
         ]
     },
 
