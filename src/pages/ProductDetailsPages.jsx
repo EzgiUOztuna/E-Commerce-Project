19124e45925d85c1eddb7052507a20c94879133d
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-06 16:38:50 +0300

    Update ProductDetailsPages.jsx

--- src/pages/ProductDetailsPages.jsx
+++ src/pages/ProductDetailsPages.jsx
@@ -23,18 +23,49 @@ export default function ProductDetailsPages() {
             </div>
 
 
-            <div className='flex flex-col'>
-                <img src={selectedImage}
-                    className='lg:w-[31rem] lg:h-[28rem]' />
+            <div className='flex lg:flex-row'>
+                <div className='flex flex-col'>
+                    <img src={selectedImage}
+                        className='lg:w-[31rem] lg:h-[28rem]' />
 
-                <div className='flex flex-col 
+                    <div className='flex flex-col 
                                 lg:flex-row'>
-                    {product.images.map((img, index) => (
-                        <img className={`lg:w-24 lg:h-20
+                        {product.images.map((img, index) => (
+                            <img className={`lg:w-24 lg:h-20
                             ${selectedImage === img ? "" : "border-transparent"}`}
-                            key={index}
-                            src={img}
-                            onClick={() => setSelectedImage(img)} />
+                                key={index}
+                                src={img}
+                                onClick={() => setSelectedImage(img)} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className='flex lg:flex-col'>
+
+                    {productDetailsData.map((item, index) => (
+                        <div key={index}
+                            className='flex flex-col'>
+                            <h4 className='font-normal text-xl leading-8 tracking-[0.2px] text-[#252B42]'>{item.h4}</h4>
+                            <div className='flex gap-3 items-center'>
+                                <div className='flex gap-1'>
+                                    <img src={item.rates.fullStar} />
+                                    <img src={item.rates.fullStar} />
+                                    <img src={item.rates.fullStar} />
+                                    <img src={item.rates.fullStar} />
+                                    <img src={item.rates.emptyStar} />
+                                </div>
+                                <h6 className='font-bold text-sm leading-6 tracking-[0.2px] text-[#737373]'>{item.rates.reviews} Reviews</h6>
+                            </div>
+                            <h3 className='font-bold text-2xl leading-8 tracking-[0.1px] text-[#252B42]'>${item.price}</h3>
+                            <div className='flex gap-1 font-bold text-sm leading-6 tracking-[0.2px]'>
+                                <h6 className='text-[#737373]'>Availability : </h6>
+                                <h6 className='text-[#23A6F0]'>{item.availability}</h6>
+                            </div>
+                            <p className='font-normal text-sm leading-5 tracking-[0.2px] text-[#858585] lg:w-[26.5rem]'>{item.desc}</p>
+
+
+
+                        </div>
                     ))}
                 </div>
             </div>
