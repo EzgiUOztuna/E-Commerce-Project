cfd4f2ef67125753eab9e9f40dd7b375009ca53c
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-06 17:57:15 +0300

    Update ProductDetailsPages.jsx

--- src/pages/ProductDetailsPages.jsx
+++ src/pages/ProductDetailsPages.jsx
@@ -11,8 +11,8 @@ export default function ProductDetailsPages() {
 
     return <>
         <SiteHeader />
-        <div className='bg-[#FAFAFA] font-montserrat flex flex-col'>
-            <div className='py-6'>
+        <div className='bg-[#FAFAFA] font-montserrat flex flex-col items-center lg:pb-0'>
+            <div className='py-16 lg:py-6'>
                 <div className='flex flex-col
                             lg:w-[69rem] lg:mx-auto'>
                     <div className='flex items-center gap-4 py-3 font-bold text-sm leading-6 tracking-[0.2px] text-center'>
@@ -23,15 +23,17 @@ export default function ProductDetailsPages() {
                 </div>
             </div>
 
-            <div className='flex mx-auto lg:flex-row lg:pb-12 lg:gap-8'>
+            <div className='flex flex-col mx-auto items-center
+                            lg:flex-row lg:pb-12 lg:gap-8 lg:items-start'>
                 <div className='flex flex-col gap-4 rounded-md'>
                     <img src={selectedImage}
-                        className='lg:w-[31rem] lg:h-[28rem]' />
+                        className='w-[348px]
+                                   lg:w-[31rem] lg:h-[28rem]' />
 
-                    <div className='flex flex-col items-center
-                                lg:flex-row lg:gap-4'>
+                    <div className='flex items-center gap-4'>
                         {product.images.map((img, index) => (
-                            <img className="lg:w-24 lg:h-20"
+                            <img className="w-28 h-20 
+                                            lg:w-24 lg:h-20"
                                 key={index}
                                 src={img}
                                 onClick={() => setSelectedImage(img)} />
@@ -42,8 +44,8 @@ export default function ProductDetailsPages() {
                 <div className='flex lg:flex-col'>
                     {productDetailsData.map((item, index) => (
                         <div key={index}
-                            className='flex flex-col lg:h-[27.8rem]'>
-                            <h4 className='font-normal text-xl leading-8 tracking-[0.2px] text-[#252B42] pb-3'>{item.h4}</h4>
+                            className='flex flex-col w-[21.75rem] px-6 lg:w-[32rem] lg:h-[27.8rem] lg:px-0'>
+                            <h4 className='font-normal text-xl leading-8 tracking-[0.2px] text-[#252B42] pb-3 pt-6 lg:pt-0'>{item.h4}</h4>
                             <div className='flex gap-3 items-center pb-3'>
                                 <div className='flex gap-1'>
                                     <img src={item.rates.fullStar} />
