287515e4cb1ce2cfca0bac24c1a3493ba08d876d
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-06 15:11:57 +0300

    Create productDetailsData.js

---
+++ src/assets/dataStore/productDetailsData.js
@@ -0,0 +1,19 @@
+export const productDetailsData = [
+    {
+        id: 1,
+        images: [
+            "src/assets/images/productDetailsImg/single-product-1-cover-2.jpg",
+            "src/assets/images/productDetailsImg/single-product-1-thumb-1.jpg",
+            "src/assets/images/productDetailsImg/single-product-1-thumb-2.jpg",
+        ]
+    },
+    {
+        id: 2,
+        images: [
+            "src/assets/images/productDetailsImg/single-product-1-cover-2.jpg",
+            "src/assets/images/productDetailsImg/single-product-1-thumb-1.jpg",
+            "src/assets/images/productDetailsImg/single-product-1-thumb-2.jpg",
+        ]
+    },
+
+];
