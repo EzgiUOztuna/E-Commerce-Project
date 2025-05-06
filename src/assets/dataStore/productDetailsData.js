5d5c3fe4423750266a2d2a0f2e9efa68dec2d627
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-06 16:38:44 +0300

    Update productDetailsData.js

--- src/assets/dataStore/productDetailsData.js
+++ src/assets/dataStore/productDetailsData.js
@@ -5,7 +5,22 @@ export const productDetailsData = [
             "src/assets/images/productDetailsImg/single-product-1-cover-2.jpg",
             "src/assets/images/productDetailsImg/single-product-1-thumb-1.jpg",
             "src/assets/images/productDetailsImg/single-product-1-thumb-2.jpg",
-        ]
+        ],
+        h4: "Floating Phone",
+        rates: {
+            fullStar: "src/assets/icons/full-stars.svg",
+            emptyStar: "src/assets/icons/empty-star.svg",
+            reviews: 10,
+        },
+        price: "1,139.33",
+        availability: "In Stock",
+        desc: "Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.",
+        colors: {
+            firstColor: "#23A6F0",
+            secondColor: "#2DC071",
+            thirdColor: "#E77C40",
+            fourthColor: "#252B42",
+        },
     },
 
 ];
