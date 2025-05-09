8d76265e272e0c5d717da8336bc2ae80849f38cd
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-09 17:29:53 +0300

    Create teamPageData.js

---
+++ public/dataStore/teamPageData.js
@@ -0,0 +1,28 @@
+export const teamPageData = [
+    {
+        desktopImg: "/images/teamPageImg/img1.png",
+        mobileImg: "/images/teamPageImg/img1-mobile.png"
+    },
+    {
+        desktopImg: "/images/teamPageImg/img2.png",
+        mobileImg: "/images/teamPageImg/img2-mobile.png"
+    },
+    {
+        desktopImg: "/images/teamPageImg/img3.png",
+        mobileImg: "/images/teamPageImg/img1-mobile.png"
+    },
+    {
+        desktopImg: "/images/teamPageImg/img3.png",
+        mobileImg: "/images/teamPageImg/img1-mobile.png"
+    },
+    {
+        desktopImg: "/images/teamPageImg/img4.png",
+        mobileImg: "/images/teamPageImg/img4-mobile.png"
+    },
+    {
+        desktopImg: "/images/teamPageImg/img5.png",
+        mobileImg: "/images/teamPageImg/img5-mobile.png"
+    },
+
+
+];
