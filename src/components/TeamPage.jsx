e666b590e129543c42baa92398ad7079910e116a
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-12 13:51:51 +0300

    Update TeamPage.jsx

--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -22,21 +22,29 @@ export default function TeamPage() {
         </div>
 
         <div className="flex flex-col justify-center items-center gap-2
-                        lg:flex-row lg:h-[505px]">
-            <img className="hidden lg:block lg:h-[530px]" src="/images/teamPageImg/img1.png" />
+                        lg:flex-row lg:h-[31.56rem]">
+            <img className="hidden lg:block lg:h-[33.125rem]" src="/images/teamPageImg/img1.png" />
             <div className="grid grid-cols-2 grid-rows-2 gap-[0.5rem] lg:h-[33.125rem]">
-                <img className="hidden lg:block lg:h-[250px]" src="/images/teamPageImg/img2.png" />
-                <img className="hidden lg:block lg:h-[250px]" src="/images/teamPageImg/img3.png" />
-                <img className="hidden lg:block lg:h-[250px]" src="/images/teamPageImg/img4.png" />
-                <img className="hidden lg:block lg:h-[250px]" src="/images/teamPageImg/img5.png" />
+                <img className="hidden lg:block lg:h-[260px]" src="/images/teamPageImg/img2.png" />
+                <img className="hidden lg:block lg:h-[260px]" src="/images/teamPageImg/img3.png" />
+                <img className="hidden lg:block lg:h-[260px]" src="/images/teamPageImg/img4.png" />
+                <img className="hidden lg:block lg:h-[260px]" src="/images/teamPageImg/img5.png" />
             </div>
 
-            <img className="block lg:hidden lg:h-[530px]" src="/images/teamPageImg/img1-mobile.png" />
+            <img className="block lg:hidden lg:h-[33.125rem]" src="/images/teamPageImg/img1-mobile.png" />
             <div className="grid grid-cols-2 grid-rows-2 gap-[0.3rem] lg:h-[33.125rem]">
                 <img className="block lg:hidden" src="/images/teamPageImg/img2-mobile.png" />
                 <img className="block lg:hidden" src="/images/teamPageImg/img3-mobile.png" />
                 <img className="block lg:hidden" src="/images/teamPageImg/img4-mobile.png" />
                 <img className="block lg:hidden" src="/images/teamPageImg/img5-mobile.png" />
+            </div>
+        </div>
+
+        <div className="flex items-center font-montserrat font-bold
+                        lg:py-28 lg:gap-28">
+            <h2 className="text-[2.5rem] leading-[3.125rem] tracking-[0.2px] text-[#252B42] mx-auto">Meet Our Team</h2>
+            <div>
+
             </div>
         </div>
 
