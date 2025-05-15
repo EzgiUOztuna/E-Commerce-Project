ab2fcc95b8c7a8b0c48bc354ab4b73b0d8192e96
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-15 14:56:19 +0300

    Update clientsData.js

--- public/dataStore/clientsData.js
+++ public/dataStore/clientsData.js
@@ -1,20 +1,20 @@
 export const clientsData = [
     {
-        img: "public/images/clientsImg/fa-brands-1.svg",
+        img: "/images/clientsImg/fa-brands-1.svg",
     },
     {
-        img: "public/images/clientsImg/fa-brands-2.svg",
+        img: "/images/clientsImg/fa-brands-2.svg",
     },
     {
-        img: "public/images/clientsImg/fa-brands-3.svg",
+        img: "/images/clientsImg/fa-brands-3.svg",
     },
     {
-        img: "public/images/clientsImg/fa-brands-4.svg",
+        img: "/images/clientsImg/fa-brands-4.svg",
     },
     {
-        img: "public/images/clientsImg/fa-brands-5.svg",
+        img: "/images/clientsImg/fa-brands-5.svg",
     },
     {
-        img: "public/images/clientsImg/fa-brands-6.svg",
+        img: "/images/clientsImg/fa-brands-6.svg",
     },
 ]
