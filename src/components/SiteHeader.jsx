b5ffda47d752c89e7d05e3891774a0dbcc2e22c2
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-15 14:25:03 +0300

    Update SiteHeader.jsx

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -14,11 +14,11 @@ export default function SiteHeader() {
                     <div className='hidden
                                 lg:flex lg:w-[25.94rem] lg:h-[2.875rem] lg:gap-[0.625rem]'>
                         <div className='lg:flex lg:w-[9.063rem] lg:h-11 lg:items-center lg:gap-[0.313rem] lg:p-[0.625rem] lg:rounded-[0.313rem]'>
-                            <img className='lg:w-4 lg:h-4' src="public/icons/phone.svg"></img>
+                            <img className='lg:w-4 lg:h-4' src="/icons/phone.svg"></img>
                             <h6 className='!font-normal lg:heading-6'>(225) 555-0118</h6>
                         </div>
                         <div className='lg:w-[16.25rem] lg:h-[2.75rem] lg:flex lg:items-center lg:gap-[0.3125rem] lg:p-[0.625rem] lg:rounded-[0.3125rem]'>
-                            <img className='lg:w-4 lg:h-4' src="public/icons/email.svg"></img>
+                            <img className='lg:w-4 lg:h-4' src="/icons/email.svg"></img>
                             <h6 className='!font-normal lg:heading-6 lg:[13.68rem] lg:h-[1.5rem]'>michelle.rivera@example.com</h6>
                         </div>
                     </div>
@@ -30,10 +30,10 @@ export default function SiteHeader() {
                     <div className='lg:flex lg:gap-[0.625rem] lg:items-center lg:justify-center lg:p-3'>
                         <h6 className='lg:heading-6'>Follow Us : </h6>
                         <div className='lg:flex lg:items-center lg:gap-[0.625rem]'>
-                            <a className='lg:p-[0.313rem]' href='/instagram'><img src='public/icons/instagram.svg'></img></a>
-                            <a className='lg:p-[0.313rem]' href='/youtube'><img src='public/icons/youtube.svg'></img></a>
-                            <a className='lg:p-[0.313rem]' href='/facebook'><img src='public/icons/facebook.svg'></img></a>
-                            <a className='lg:p-[0.313rem]' href='/twitter'><img src='public/icons/twitter.svg'></img></a>
+                            <a className='lg:p-[0.313rem]' href='/instagram'><img src='/icons/instagram.svg'></img></a>
+                            <a className='lg:p-[0.313rem]' href='/youtube'><img src='/icons/youtube.svg'></img></a>
+                            <a className='lg:p-[0.313rem]' href='/facebook'><img src='/icons/facebook.svg'></img></a>
+                            <a className='lg:p-[0.313rem]' href='/twitter'><img src='/icons/twitter.svg'></img></a>
                         </div>
                     </div>
                 </div>
@@ -53,7 +53,7 @@ export default function SiteHeader() {
                         <a className='middle-nav-links
                                     lg:hidden' href='/price'>Pricing</a>
                         <a className='hidden
-                                    lg:link-style lg:flex lg:gap-1' onClick={toggleDropdown}>Shop<img src="public/icons/dropdown.svg"></img></a>
+                                    lg:link-style lg:flex lg:gap-1' onClick={toggleDropdown}>Shop<img src="/icons/dropdown.svg"></img></a>
                         <a className='hidden
                                     lg:block lg:link-style' href='/about'>About</a>
                         <a className='hidden
@@ -91,19 +91,19 @@ export default function SiteHeader() {
                 <div className='w-1/2 order-2 flex items-center gap-[0.5rem]
                                 lg:text-[#23A6F0] lg:gap-0 lg:order-3 lg:max-w-[19.06rem]'>
                     <a href="/login" className='lg:navbar-actions'>
-                        <img className='hidden lg:block' src='public/icons/user.svg'></img>
-                        <img className='block lg:hidden' src='public/icons/user-mobile.svg'></img>
+                        <img className='hidden lg:block' src='/icons/user.svg'></img>
+                        <img className='block lg:hidden' src='/icons/user-mobile.svg'></img>
                         <p className='hidden
                                     lg:block lg:text-center lg:text-[#23A6F0] lg:font-bold lg:text-[0.875rem] lg:leading-[1.5rem] lg:tracking-[0.0125rem] lg:font-montserrat'>Login / Register</p>
                     </a>
-                    <a href='/search'><img className='hidden lg:navbar-actions' src='public/icons/search.svg'></img></a>
-                    <a href='/search'><img className='block lg:hidden' src='public/icons/search-mobile.svg'></img></a>
+                    <a href='/search'><img className='hidden lg:navbar-actions' src='/icons/search.svg'></img></a>
+                    <a href='/search'><img className='block lg:hidden' src='/icons/search-mobile.svg'></img></a>
 
-                    <a href='/cart'><img className='hidden lg:navbar-actions' src='public/icons/cart.svg'></img></a>
-                    <a href='/cart'><img className='block lg:hidden' src='public/icons/cart-mobile.svg'></img></a>
+                    <a href='/cart'><img className='hidden lg:navbar-actions' src='/icons/cart.svg'></img></a>
+                    <a href='/cart'><img className='block lg:hidden' src='/icons/cart-mobile.svg'></img></a>
 
-                    <a href='/favorites'><img className='hidden lg:navbar-actions' src='public/icons/heart.svg'></img></a>
-                    <a href='/menu'><img className='block lg:hidden' src='public/icons/hamburger.svg'></img></a>
+                    <a href='/favorites'><img className='hidden lg:navbar-actions' src='/icons/heart.svg'></img></a>
+                    <a href='/menu'><img className='block lg:hidden' src='/icons/hamburger.svg'></img></a>
                 </div>
             </div>
         </div >
