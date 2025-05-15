fcb74a3d08f8dab29bf09cb5e8303146ef4402bf
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-15 15:13:23 +0300

    Create aboutUsData.js

---
+++ public/dataStore/aboutUsData.js
@@ -0,0 +1,22 @@
+export const aboutUsData = [
+    {
+        id: 1,
+        number: "15K",
+        def: "Happy Customers",
+    },
+    {
+        id: 2,
+        number: "150K",
+        def: "Monthly Visitors",
+    },
+    {
+        id: 3,
+        number: "15",
+        def: "Countries Worldwide",
+    },
+    {
+        id: 4,
+        number: "100+",
+        def: "Top Partners",
+    },
+]
