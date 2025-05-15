0a7437ff62518d5a3647cdb90c94e549ae106b51
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-15 14:25:04 +0300

    Update InnerPages.jsx

--- src/pages/InnerPages.jsx
+++ src/pages/InnerPages.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import ContactPage from "../components/ContactPage";
 import TeamPage from "../components/TeamPage";
+import AboutUsPage from "../components/AboutUsPage";
 
 export default function InnerPages() {
     return <>
         <Routes>
             <Route path="contact" element={<ContactPage />} />
             <Route path="team" element={<TeamPage />} />
+            <Route path="about" element={<AboutUsPage />} />
         </Routes>
     </>
 }
