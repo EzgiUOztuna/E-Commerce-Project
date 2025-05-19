2177a93d66cc636bd697c2cf6ef7bcb228a6cf32
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-19 11:32:18 +0300

    Update SignUpPage.jsx

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useForm } from 'react-hook-form';
+
 
 export default function SignUpPage() {
+    const { register,
+        handleSubmit,
+        watch,
+        formState: { errors } } = useForm();
+
+    /*
+    https://ozmeneyup60.medium.com/react-jste-form-y%C3%B6netimi-react-hook-form-vs-formik-e6d394f30057
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         password: '',
         verifiedPassword: ''
     });
+    */
 
     return <>
 
