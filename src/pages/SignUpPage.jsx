883640e81c59c5df6948e6105b5696bff0965d09
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-21 14:39:43 +0300

    Update SignUpPage.jsx

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,11 +1,12 @@
 import { useForm } from 'react-hook-form';
 
-
 export default function SignUpPage() {
     const { register,
         handleSubmit,
         watch,
         formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
 
     /*
     https://ozmeneyup60.medium.com/react-jste-form-y%C3%B6netimi-react-hook-form-vs-formik-e6d394f30057
@@ -18,6 +19,21 @@ export default function SignUpPage() {
     */
 
     return <>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type='text'
+                placeholder='First name'
+                {...register("First name", { required: true, min: 3, maxLength: 80 })} />
+            <input type='text'
+                placeholder='Last name'
+                {...register("Last name", { required: true, maxLength: 100 })} />
+            <input type='text'
+                placeholder='Email'
+                {...register("Email", { required: true, pattern: { value: /^\S+@\S+$/i, message: 'Invalid email address' } })} />
+            {errors.email && <div>{errors.email.message}</div>}
+            <input type='text'
+                placeholder='Password'
+                {...register('Password', { required: true, minLength: 8, pattern: { value: '(?=.*)(?=.*[a-z])(?=.*[A-Z]).{8,}' }, message: 'Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters' })} />
+        </form>
 
     </>;
 }
