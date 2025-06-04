f2863249ea87befec72714f2abf1c3c301c43fe2
EzgiUOztuna <ezgiuygn@gmail.com>
2025-06-04 16:44:37 +0300

    Create clientReducer.js

---
+++ src/store/reducers/clientReducer.js
@@ -0,0 +1,12 @@
+const clientInitial = {
+    user: {},
+    addressList: [],
+    creditCards: [],
+    roles: [],
+    theme: '',
+    language: '',
+}
+
+const clientReducer = (state = clientInitial, action = {}) => {
+    return state;
+}
