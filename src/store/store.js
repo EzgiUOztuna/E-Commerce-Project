c8aff9d82ec825d26d55e4b5d1d15a06edfa173f
EzgiUOztuna <ezgiuygn@gmail.com>
2025-06-04 17:14:40 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, legacy_createStore as createStore } from "redux";
 import { clientReducer } from "./reducers/clientReducer";
 import { productReducer } from "./reducers/productReducer";
+import { shoppingCartReducer } from "./reducers/shoppingCartReducer";
 
 export const reducers = combineReducers({
     clients: clientReducer,
     products: productReducer,
+    shoppingCart: shoppingCartReducer,
 })
 
 export const store = createStore(reducers);
