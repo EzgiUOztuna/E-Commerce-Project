33fe31ca9f205f73c967f80f4b3547bd58176e24
EzgiUOztuna <ezgiuygn@gmail.com>
2025-06-04 17:05:24 +0300

    Update clientReducer.js

--- src/store/reducers/clientReducer.js
+++ src/store/reducers/clientReducer.js
@@ -1,3 +1,10 @@
+export const ClientActions = {
+    setUser: 'SET_USER',
+    setRoles: 'SET_ROLES',
+    setTheme: 'SET_THEME',
+    setLanguage: 'SET_LANGUAGE',
+};
+
 const clientInitial = {
     user: {},
     addressList: [],
@@ -7,6 +14,14 @@ const clientInitial = {
     language: '',
 }
 
-const clientReducer = (state = clientInitial, action = {}) => {
-    return state;
+export const clientReducer = (state = clientInitial, action = {}) => {
+    switch (action.type) {
+        case ClientActions.setUser:
+            return { ...state, user: action.payload };
+        case ClientActions.setRoles:
+            return { ...state, roles: action.payload };
+        // ⚠️ Diğerleri de gelecek
+        default:
+            return state;
+    }
 }
