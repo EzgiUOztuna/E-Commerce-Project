fb19cf6e240e4edf13321db0fa0dea94528efa37
EzgiUOztuna <ezgiuygn@gmail.com>
2025-06-04 16:50:43 +0300

    Update productReducer.js

---
+++ src/store/reducers/productReducer.js
@@ -0,0 +1,18 @@
+export const ProductActions = {
+    setCategories: 'SET_CATEGORIES',
+    setProductList: 'SET_PRODUCT_LIST',
+    setTotal: 'SET_TOTAL',
+    setFetchState: 'SET_FETCH_STATE',
+    setLimit: 'SET_LIMIT',
+    setOffset: 'SET_OFFSET',
+    setFilter: 'SET_FILTER'
+}
+
+export const productReducer = (state, action) => {
+    switch (action.type) {
+        case value:
+            break;
+        default:
+            break;
+    }
+}
