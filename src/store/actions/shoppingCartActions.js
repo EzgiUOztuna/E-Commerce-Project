f6548c38fdec71fec74b844cdfa42a67daab0fff
EzgiUOztuna <ezgiuygn@gmail.com>
2025-08-06 17:25:17 +0300

    Create shoppingCartActions.js

---
+++ src/store/actions/shoppingCartActions.js
@@ -0,0 +1,15 @@
+/*
+export const ShoppingCartActions = {
+    setCart: 'SET_CART',
+    setPayment: 'SET_PAYMENT',
+    setAddress: 'SET_ADDRESS',
+}
+*/
+
+export const SET_CART = 'setCart';
+export const SET_PAYMENT = 'setPayment';
+export const SET_ADDRESS = 'setAddress';
+
+export function setCart(value) {
+    return { type: 'SET_CART', payload: value };
+}
