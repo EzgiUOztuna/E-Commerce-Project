73a2a13104daaf55dcdceac19fde11e06c4f9269
EzgiUOztuna <ezgiuygn@gmail.com>
2025-08-07 16:01:29 +0300

    adding comments

--- src/store/actions/clientActions.js
+++ src/store/actions/clientActions.js
@@ -1,22 +1,14 @@
-/*
-export const ClientActions = {
-    setUser: 'SET_USER',
-    setRoles: 'SET_ROLES',
-    setTheme: 'SET_THEME',
-    setLanguage: 'SET_LANGUAGE',
-};
-*/
-
 export const SET_USER = 'setUser';
 export const SET_ROLES = 'setRoles';
 export const SET_THEME = 'setTheme';
 export const SET_LANGUAGE = 'setLanguage';
 
-export function setUser(value) {
+//ACTION CREATOR FUNCTIONS ⤵️
+export function setUser(value) { //value yerine newUser gibi şeyler de yazabilirim.
     return { type: SET_USER, payload: value };
 }
 
-export function setRoles(value) {
+export function setRoles(value) { //export const setRoles = (value) => { return ...}
     return { type: SET_ROLES, payload: value };
 }
 
@@ -27,3 +19,19 @@ export function setTheme(value) {
 export function setLanguage(value) {
     return { type: SET_LANGUAGE, payload: value };
 }
+
+
+
+
+
+
+
+
+/*
+export const ClientActions = {
+    setUser: 'SET_USER',
+    setRoles: 'SET_ROLES',
+    setTheme: 'SET_THEME',
+    setLanguage: 'SET_LANGUAGE',
+};
+*/
