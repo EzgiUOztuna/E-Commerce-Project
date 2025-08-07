c1b81d4a30e6190bed6bb6133849041216cc0d0c
EzgiUOztuna <ezgiuygn@gmail.com>
2025-08-07 16:01:14 +0300

    changing store name

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './store/store.js';
+import { myStore } from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <Provider store={myStore}> {/* React-Redux arasındaki bağlantıyı kurmak için Provider kullanıyoruz. */}
     <BrowserRouter>
       <App />
     </BrowserRouter>,
